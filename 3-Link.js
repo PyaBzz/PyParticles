4238c1219efaf1b5790f65023728a02daf87c534
Administrator <Pooya>
2018-01-28 19:16:26 +0000

    Clean-up.

--- 3-Link.js
+++ 3-Link.js
@@ -10,23 +10,11 @@ link.prototype.calculate_forces = function () {
 	var diff_x = this.p1.x - this.p2.x;
 	var diff_y = this.p1.y - this.p2.y;
 	var diff_z = this.p1.z - this.p2.z;
-	// var length = Math.sqrt(Math.pow(diff_x,2) + Math.pow(diff_y,2) + Math.pow(diff_z,2));
-	// var twoD_length = Math.sqrt(Math.pow(diff_x,2) + Math.pow(diff_y,2));
-	// var z_sin = diff_z / length;
-	// var z_cos = twoD_length / length;
-	// var twoD_cos = diff_x / twoD_length;
-	// var twoD_sin = diff_y / twoD_length;
+	var twoD_length = Math.sqrt(Math.pow(diff_x,2) + Math.pow(diff_y,2));
 	
-    // if (tearable && twoD_length > link_tearing_length) this.p1.remove_links(this);  // 2D
-    if (tearable && length > link_tearing_length) this.p1.remove_links(this);  // 3D
+    if (tearable && twoD_length > link_tearing_length) this.p1.remove_links(this);  // 2D
+    // if (tearable && length > link_tearing_length) this.p1.remove_links(this);  // 3D
 	
-	// var stretch_length = Math.max(length - resting_link_length, 0);  // A stretching cloth only reacts to stretch not to compression
-	// var stretch_length = length - resting_link_length;  // An elastic surface at rest resists stretch but doesn't mind compression
-	// var stretch_length = length;  // An already stretched elastic surface not only resists stretch but also tries to contract
-
-	// var nonlinear_effective_stretch_length = Math.pow(stretch_length, nonlinearity);
-	// var twoD_nonlinear_effective_stretch_length = nonlinear_effective_stretch_length * z_cos;
-
     var reaction_force_x = Math.sign(diff_x)*Math.pow(Math.abs(diff_x),nonlinearity) * elastic_stiffness;
     var reaction_force_y = Math.sign(diff_y)*Math.pow(Math.abs(diff_y),nonlinearity) * elastic_stiffness;
     var reaction_force_z = diff_z * elastic_stiffness;
