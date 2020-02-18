323081a6f10e6c332b71ee27dea6d85e61e40919
PyaBzz <>
2020-02-18 12:11:59 +0000

    Set right-click action default to cut

--- 09-Config.js
+++ 09-Config.js
@@ -20,7 +20,7 @@ waterConfig = {
     nodeRadius: 0,  // in pixels. 0 to disable node drawing
     pinColour: "red",
     mouseImpactRadius: 2, // diameter of mouse impact in number of cells
-    rightClickAction: 1, // 0 = cut, 1 = mark
+    rightClickAction: 0, // 0 = cut, 1 = mark
     mouseCuttingRadius: 2, // diameter of mouse cutting in number of cells
     mouseSlipFactor: 0.6, // [0-1] 0 = maximum slip, 1 = no slip
 };
